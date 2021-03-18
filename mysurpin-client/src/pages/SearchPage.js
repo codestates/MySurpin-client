905f486adeaa63effec12d229b6c2f800b7a9805
JewonYeon <yjw9852@gmail.com>
2021-03-18 17:14:08 +0900

    [Client] add redux store/actions/reducers

--- mysurpin-client/src/pages/SearchPage.js
+++ mysurpin-client/src/pages/SearchPage.js
@@ -2,37 +2,43 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Surpin from "../components/Surpin";
 import SearchResult from "../components/SearchResult";
-
 const SearchPage = () => {
   return (
-    <div className="searchBar">
+    <>
       <Navbar></Navbar>
-      <div className="searchbar">
-        <input className="sarchbar__input"></input>
-        <button className="sarchbar__button">
-          <img src=""></img>
-        </button>
-      </div>
-      <div className="searchpage-best-results">
-        <div className="searchpage__best__title"></div>
-        <ul className="searchpage-best-lists">
-          <li className="searchpage-best-list">
-            <Surpin></Surpin>
-          </li>
-        </ul>
-      </div>
-      <div className="searchpage-all-results">
-        <div className="searchpage__all__title"></div>
-        <div className="searchpage__all__lists">
-          <ul className="searchpage-all-results__lists">
-            <li className="searchpage-all-result__list">
-              <SearchResult></SearchResult>
+      <div className="searchPage">
+        <div className="searchbar">
+          <input className="sarchbar__input" placeholder="search for"></input>
+          <button className="sarchbar__button">
+            <img src="../../public/images/search.png"></img>
+          </button>
+        </div>
+        <div className="searchpage-best-results">
+          <div className="searchpage__best__title">Best Surpins</div>
+          <ul className="searchpage-best-lists">
+            <li className="searchpage-best-list">
+              <Surpin></Surpin>
             </li>
           </ul>
         </div>
+        <div className="searchpage-all-results">
+          <div className="searchpage__all__title">All Surpins</div>
+          <div className="searchpage__all__lists">
+            <div className="searchpage__all__lists__topbar">
+              <div className="topbar__name"></div>
+              <div className="topbar__description"></div>
+              <div className="topbar__numbOfUrls"></div>
+              <div className="topbar__createdAt"></div>
+            </div>
+            <ul className="searchpage-all-results__lists">
+              <li className="searchpage-all-result__list">
+                <SearchResult></SearchResult>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
-
 export default SearchPage;
