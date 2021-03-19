4af2aefa2031a8318163d7b585b710c74e9bc1ab
JewonYeon <yjw9852@gmail.com>
2021-03-19 15:38:41 +0900

    delete

--- mysurpin-client/src/reducers/surpinReducer.js
+++ mysurpin-client/src/reducers/surpinReducer.js
@@ -4,13 +4,13 @@ import {
   SHOW_USER_LISTS,
   SHOW_SURPIN,
 } from "../actions/index";
-import { surpinState } from "./initialState";
+import { initialState } from "./initialState";
 
-const createMySurpin = (state = surpinState, action) => {
+const createMySurpin = (state = initialState, action) => {
   switch (action.type) {
     case GET_NEW_LISTS:
       return Object.assign({}, state, {
-        newLists: action.payload.data,
+        newLists: "서버에서 받아온 값",
       });
 
     case GET_BEST_TAGS:
