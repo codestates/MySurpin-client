cf192bfc939a838fda30381364ce8972a1678c33
JewonYeon <yjw9852@gmail.com>
2021-03-21 17:55:54 +0900

    [Client] redux-persist complete

--- mysurpin-client/src/store/store.js
+++ mysurpin-client/src/store/store.js
@@ -1,6 +1,7 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/index";
 import thunk from "redux-thunk";
+import { persistStore } from "redux-persist";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
   ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
@@ -9,5 +10,6 @@ const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
+const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
