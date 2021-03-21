708332fe0c07af18bdad27d9e2cafda08b55e5a7
yuntaek ha <yuntaek.ha@gmail.com>
2021-03-21 23:28:59 +0900

    [Client] add custom hooks

--- mysurpin-client/src/components/BesttagsSection.js
+++ mysurpin-client/src/components/BesttagsSection.js
@@ -2,20 +2,16 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Line } from "@reactchartjs/react-chart.js";
 
-const BesttagsSection = ({
-  animatedItem,
-  useScrollEventListener,
-  chartdata,
-  handleChartdata,
-}) => {
+const BesttagsSection = ({ animatedItem, chartdata }) => {
   const state = useSelector((state) => state.surpinReducer);
   const { tags } = state;
+
   let data = {
     labels: chartdata,
     datasets: [
       {
         label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
+        data: chartdata,
         fill: true,
         backgroundColor: "rgba(75,192,192,0.2)",
         borderColor: "rgba(75,192,192,1)",
@@ -44,5 +40,4 @@ const BesttagsSection = ({
     </div>
   );
 };
-
 export default BesttagsSection;
--- mysurpin-client/src/hooks/useScrollEventListener.js.js
+++ mysurpin-client/src/hooks/useScrollEventListener.js.js
@@ -1,11 +1,10 @@
 import { useRef, useEffect, useCallback } from "react";
 
-const useScrollEventListener = (func, threshold) => {
+const useScrollEventListener = (func, threshold, style) => {
   const dom = useRef();
 
   const handleScroll = useCallback(([entry]) => {
     const { current } = dom;
-
     if (entry.isIntersecting) {
       func();
     }
@@ -14,18 +13,15 @@ const useScrollEventListener = (func, threshold) => {
   useEffect(() => {
     let observer;
     const { current } = dom;
-
     if (current) {
       observer = new IntersectionObserver(handleScroll, { threshold });
       observer.observe(current);
-
       return () => observer && observer.disconnect();
     }
   }, [handleScroll]);
-
   return {
     ref: dom,
+    style,
   };
 };
-
 export default useScrollEventListener;
